fe9bfa95713bbaee274321e39e996e938f591982
Francisco Acha <facha@riam.local>
2021-01-13 12:00:48 +0100

    script anyadido

---
+++ script.bat
@@ -0,0 +1,9 @@
+@echo off
+cd %cd%
+move *.* "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp"
+git init
+git add . -A
+move "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp\*.*" %cd% 
+git add .
+git commit -m "Renombrar"
+git push origin master
