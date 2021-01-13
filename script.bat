53175d5c39ff330a149e0c6ba1c9384729f1d9e7
Francisco Acha <facha@riam.local>
2021-01-13 12:11:48 +0100

    cambios en script

--- script.bat
+++ script.bat
@@ -1,9 +1,9 @@
 @echo off
 cd %cd%
-move *.* "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp"
-git init
-git add . -A
-move "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp\*.*" %cd% 
-git add .
-git commit -m "Renombrar"
-git push origin master
+move "%cd%\*" "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp"
+::git init
+::git add . -A
+::move "C:\Users\facha\Documents\Visual Studio 2015\Projects\temp\*.*" %cd% 
+::git add .
+::git commit -m "Renombrar"
+::git push origin master
